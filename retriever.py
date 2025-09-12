41053b05f92429f278cc4f82d1366865242e4f0b
Pritiks23 <65793273+Pritiks23@users.noreply.github.com>
2025-09-12 13:34:49 -0700

    Update retriever.py

--- retriever.py
+++ retriever.py
@@ -6,7 +6,7 @@ class InMemoryRetriever:
         self.docs = docs
         self.embeddings = embeddings
 
-    def retrieve(self, query_embedding, top_k=2):
+    def retrieve(self, query_embedding, top_k=1):
         sims = cosine_similarity([query_embedding], self.embeddings)[0]
         idxs = np.argsort(sims)[::-1][:top_k]
         return [self.docs[i] for i in idxs]
